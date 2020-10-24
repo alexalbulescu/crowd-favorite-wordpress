237e70da56610d163ed84919a42b0924c5bb8a6f
Alex Albulescu <albulescu.victor.alexandru@gmail.com>
2020-10-24 10:59:11 +0300

    [H3.2] - actions and filters

--- test-crowd-favorite.php
+++ test-crowd-favorite.php
@@ -10,3 +10,19 @@
 define( 'CFAV_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 require_once( CFAV_PLUGIN_DIR . 'class.cfav-menu-walker.php' );
+
+// example of an action
+function cfav_action_example() { 
+    echo '<div style="background: #0073aa; color: white; text-align: center;">Example of action.</div>'; 
+}
+
+add_action('wp_footer', 'cfav_action_example'); 
+
+// example of a filter
+function cfav_css_body_class( $classes ) {
+    if ( ! is_admin() ) {
+        $classes[] = 'cfav-test-class';
+    }
+    return $classes;
+}
+add_filter( 'body_class', 'cfav_css_body_class' );
